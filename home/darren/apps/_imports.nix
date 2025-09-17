b64dfc3b5d0f2b3eb67f8c295fbe2e42736687fc
Darren Gregory <darrenjgregory@gmail.com>
2025-09-17 16:05:51 +0100

    Fix _imports.nix to return a list

--- home/darren/apps/_imports.nix
+++
@@ -1,7 +0,0 @@
-# home/darren/apps/_imports.nix
-let
-  dir   = ./.;
-  files = builtins.attrNames (builtins.readDir dir);
-  nixFiles = builtins.filter (f: f != "_imports.nix" && builtins.match ".*\\.nix" f != null) files;
-in map (f: dir + ("/" + f)) nixFiles
-
