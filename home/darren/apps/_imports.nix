3dfd423becb50fafbf4ea0bb53c5d08b34e97f95
Darren Gregory <darrenjgregory@gmail.com>
2025-09-17 13:46:02 +0100

    fix: ensure _imports.nix returns modules instead of paths

--- home/darren/apps/_imports.nix
+++ home/darren/apps/_imports.nix
@@ -1,7 +1,7 @@
+# home/darren/apps/_imports.nix
 let
   dir   = ./.;
   files = builtins.attrNames (builtins.readDir dir);
-  nixFiles = builtins.filter (f:
-    f != "_imports.nix" && builtins.match ".*\\.nix" f != null
-  ) files;
+  nixFiles = builtins.filter (f: f != "_imports.nix" && builtins.match ".*\\.nix" f != null) files;
 in map (f: dir + ("/" + f)) nixFiles
+
