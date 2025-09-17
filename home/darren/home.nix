93361c333680d954f40302df237f7609897722a3
Darren Gregory <darrenjgregory@gmail.com>
2025-09-17 16:03:59 +0100

    Fix _imports.nix to return a list

--- home/darren/home.nix
+++ home/darren/home.nix
@@ -5,42 +5,18 @@
   home.homeDirectory = "/home/darren";
   home.stateVersion = "25.05";
 
-imports = [
-  ./apps/bitwarden.nix
-  ./apps/obsidian.nix
-  ./apps/signal.nix
-  ./apps/vivaldi.nix
-  ./apps/vscode.nix
+  imports = [
+    ./apps/vivaldi.nix
+    ./apps/bitwarden.nix
+    ./apps/signal.nix
+    ./apps/obsidian.nix
+    ./apps/vscode.nix
 
-  ./gaming/steam.nix
-  ./gaming/gamemode.nix
+    ./gaming/steam.nix
+    ./gaming/gamemode.nix
 
-  ./dev/tools.nix
-imports = [
-  ./apps/bitwarden.nix
-  ./apps/obsidian.nix
-  ./apps/signal.nix
-  ./apps/vivaldi.nix
-  ./apps/vscode.nix
-
-  ./gaming/steam.nix
-  ./gaming/gamemode.nix
-
-  ./dev/tools.nix
-];
-imports = [
-  ./apps/bitwarden.nix
-  ./apps/obsidian.nix
-  ./apps/signal.nix
-  ./apps/vivaldi.nix
-  ./apps/vscode.nix
-
-  ./gaming/steam.nix
-  ./gaming/gamemode.nix
-
-  ./dev/tools.nix
-];
-
+    ./dev/tools.nix
+  ];
 
   programs.git = {
     enable = true;
@@ -50,18 +26,4 @@ imports = [
 
   programs.home-manager.enable = true;
   programs.zsh.enable = true;
-
-  # App toggles
-  hmApps.vivaldi.enable   = true;
-  hmApps.bitwarden.enable = true;
-  hmApps.bitwarden.cli    = true;
-  hmApps.signal.enable    = true;
-  hmApps.obsidian.enable  = true;
-  hmApps.vscode.enable    = true;
-
-  # Gaming toggles
-
-  # Enable some gaming tools
-  
 }
-
