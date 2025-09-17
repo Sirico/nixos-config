3ca0530906f880f8957b389373e1c5da8e986ee4
Darren Gregory <darrenjgregory@gmail.com>
2025-09-17 12:18:00 +0100

    hosts/nitro-5: sync hardware-configuration, set stateVersion, fix EFI mountpoint

--- hosts/nitro-5/configuration.nix
+++ hosts/nitro-5/configuration.nix
@@ -5,13 +5,14 @@
     ./hardware-configuration.nix
     ../../modules/base.nix
   ];
+  system.stateVersion = "25.05";
 
   # Make boot explicit for this host
   boot.loader.grub.enable = false;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   # Host-only bits
   networking.hostName = "nitro-5";
