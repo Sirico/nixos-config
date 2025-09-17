a1d57b9026fdbaaf076b2dede6228d95634919e4
Darren Gregory <darrenjgregory@gmail.com>
2025-09-17 15:32:53 +0100

    fix: correct syntax in gaming/_imports.nix

--- home/darren/gaming/_imports.nix
+++ home/darren/gaming/_imports.nix
@@ -7,5 +7,4 @@ let
   nixFiles = builtins.filter (f:
     f != "_imports.nix" && builtins.match ".*\\.nix" f != null
   ) files;
-in
-  builtins.map (f: import (dir + "/" + f)) nixFiles
+in map (f: import (dir + ("/" + f))) nixFiles
